f5ec269d222ee3f98aa65be307987cf774e6d6c9
Stephen Mildenhall <steve@mynl.com>
2022-12-14 10:17:16 +0000

    0.9.6.3 build

--- setup.py
+++ setup.py
@@ -25,22 +25,71 @@ install_requires = [
     'sly',
     'titlecase',
     # docs
-    'docutils',
-    'jupyter-sphinx',
-    'nbsphinx',
-    'recommonmark',
-    'setuptools',
-    'sphinx',
-    'sphinx-panels',
-    'sphinx-rtd-dark-mode',
-    'sphinxcontrib-bibtex',
-    'sphinx-copybutton',
-    'sphinx-toggleprompt',
+    # 'docutils',
+    # 'jupyter-sphinx',
+    # 'nbsphinx',
+    # 'recommonmark',
+    # 'setuptools',
+    # 'sphinx',
+    # 'sphinx-panels',
+    # 'sphinx-rtd-dark-mode',
+    # 'sphinxcontrib-bibtex',
+    # 'sphinx-copybutton',
+    # 'sphinx-toggleprompt',
     'IPython'
 ]
 
 
-long_description = Path('Long_description.rst').read_text(encoding='utf-8')
+long_description = """aggregate: a powerful aggregate distribution modeling library in Python
+========================================================================
+
+What is it?
+-----------
+
+**aggregate** is a Python package providing an expressive language and fast,
+accurate computations to make working with aggregate (compound) probability
+distributions easy and intuitive. It allows students and practitioners to
+use realistic real-world distributions that reflect the underlying
+frequency and severity generating processes. It has applications in
+insurance, risk management, actuarial science, and related areas.
+
+Documentation
+-------------
+
+https://aggregate.readthedocs.io/
+
+
+Where to get it
+---------------
+
+https://github.com/mynl/aggregate
+
+
+Installation
+------------
+
+::
+
+  pip install aggregate
+
+
+Dependencies
+------------
+
+See requirements.txt.
+
+License
+-------
+
+BSD 3 licence
+
+Contributing to aggregate
+-------------------------
+
+All contributions, bug reports, bug fixes, documentation improvements,
+enhancements and ideas are welcome.
+
+"""
 
 version = aggregate.__version__
 
--- twine_update.bat
+++ twine_update.bat
@@ -5,8 +5,10 @@ REM twine upload dist/aggregate-0.9.5.1.*
 REM internet says
 REM twine upload dist/*
 
-twine check dist/aggregate-0.9.6.1*
+echo %1
 
-twine upload -u mildenhall -p %PPPW% dist/*0.9.6.1* --verbose
+twine check dist/aggregate-%1*
+
+twine upload -u mildenhall -p %PPPW% dist/*%1* --verbose
 
 
